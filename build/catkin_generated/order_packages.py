97d2009b095889cdc9ee7b29f95e625c545eaa8c
root <ariantavoos@gmail.com>
2024-11-29 16:55:46 +0000

    Turtle package

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/catkinws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/catkinws/devel;/opt/ros/noetic'.split(';') if '/catkinws/devel;/opt/ros/noetic' != '' else []
